f5b3d71c839547a75a2b6423cbb8cd2b6cff2125
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-13 10:30:48 +0530

    conftest added

--- Test_Cases/conftest.py
+++ Test_Cases/conftest.py
@@ -24,38 +24,38 @@ def log_on_failure(request, get_browser):
         allure.attach(driver.get_screenshot_as_png(), name="do_login", attachment_type=AttachmentType.PNG)
 
 
-@pytest.fixture(params=["firefox", "chrome", ], scope="function")
-def get_browser(request):
-    if request.param == "chrome":
-        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    if request.param == "firefox":
-        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    request.cls.driver = driver
-    driver.get(configuration_reader("basic configuration", "test_url"))
-    driver.maximize_window()
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
-
-# @pytest.fixture(params=["firefox", "chrome"], scope="function")
+# @pytest.fixture(params=["firefox", "chrome", ], scope="function")
 # def get_browser(request):
-#     sauce_url = "https://oauth-animesh5678-8230c:9bdf2d99-49a6-4098-ae67-831f601b4b2f@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
-#     desired_cap = {}
-#     desired_cap['name'] = 'Dem_Web_Shop_Test'
 #     if request.param == "chrome":
-#         desired_cap['browserName'] = 'chrome'
-#         desired_cap['platform'] = 'Windows 10'
-#         desired_cap['version'] = 'latest'
-#         driver = webdriver.Remote(sauce_url, desired_capabilities=desired_cap)
-#     elif request.param == "firefox":
-#         desired_cap['browserName'] = 'firefox'
-#         desired_cap['platform'] = 'Windows 10'
-#         desired_cap['version'] = 'latest'
-#         driver = webdriver.Remote(sauce_url, desired_capabilities=desired_cap)
+#         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+#     if request.param == "firefox":
+#         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 #     request.cls.driver = driver
 #     driver.get(configuration_reader("basic configuration", "test_url"))
-#     driver.implicitly_wait(10)
 #     driver.maximize_window()
+#     driver.implicitly_wait(10)
 #     yield driver
 #     driver.quit()
+
+@pytest.fixture(params=["firefox", "chrome"], scope="function")
+def get_browser(request):
+    sauce_url = "https://oauth-animesh5678-8230c:9bdf2d99-49a6-4098-ae67-831f601b4b2f@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
+    desired_cap = {}
+    desired_cap['name'] = 'Dem_Web_Shop_Test'
+    if request.param == "chrome":
+        desired_cap['browserName'] = 'chrome'
+        desired_cap['platform'] = 'Windows 10'
+        desired_cap['version'] = 'latest'
+        driver = webdriver.Remote(sauce_url, desired_capabilities=desired_cap)
+    elif request.param == "firefox":
+        desired_cap['browserName'] = 'firefox'
+        desired_cap['platform'] = 'Windows 10'
+        desired_cap['version'] = 'latest'
+        driver = webdriver.Remote(sauce_url, desired_capabilities=desired_cap)
+    request.cls.driver = driver
+    driver.get(configuration_reader("basic configuration", "test_url"))
+    driver.implicitly_wait(10)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
 
